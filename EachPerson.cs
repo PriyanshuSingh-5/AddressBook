1fe990c84634d889be9f64da9225e587649dc9d3
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-19 18:06:00 +0530

    [Priyanshu]modified Delete contact

--- EachPerson.cs
+++ EachPerson.cs
@@ -8,8 +8,9 @@ namespace AddressBook
 {
     interface EachPerson
     {
-        void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
-        void EditContact(string name);
+       public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        public void EditContact(string name);
+        public void DeleteContact(string name);
 
     }
     }
