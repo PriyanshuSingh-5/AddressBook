339f59cd1d4e857270748db9595e130f8559aa8e
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-19 17:48:43 +0530

    [Priyanshu]modified edit contact details

--- ContactDetails.cs
+++ ContactDetails.cs
@@ -13,8 +13,8 @@ namespace AddressBook
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string Zip { get; set; }
-        public int Email { get; set; }
+        public int Zip { get; set; }
+        public string Email { get; set; }
         public long PhoneNumber { get; set; }
     }
 }
