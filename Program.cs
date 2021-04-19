f84f75b0d6c77b7d55110b526cfc3911fa8eb5bf
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-19 17:27:41 +0530

    [Priyanshu]modified add details

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address book");
+            ContactDetails person = new ContactDetails("Priyanshu", "Singh", "yercaud", "Salem", "TamilNadu", "636601", "9003530893", "priyanshuSVS2015@gmail.com");
+
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            Console.Read();
+
         }
     }
 }
