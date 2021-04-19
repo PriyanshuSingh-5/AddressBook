260c9166336959c204566cf54c03eb756e76796b
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-19 17:41:48 +0530

    [Priyanshu]modified contact details

--- Program.cs
+++ Program.cs
@@ -6,19 +6,28 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address book");
-            ContactDetails person = new ContactDetails("Priyanshu", "Singh", "yercaud", "Salem", "TamilNadu", "636601", "9003530893", "priyanshuSVS2015@gmail.com");
-
-            Console.WriteLine(person.getName());
-            Console.WriteLine(person.getlastName());
-            Console.WriteLine(person.getaddress());
-            Console.WriteLine(person.getcity());
-            Console.WriteLine(person.getstate());
-            Console.WriteLine(person.getzip());
-            Console.WriteLine(person.getphone());
-            Console.WriteLine(person.getemail());
+            Console.WriteLine("-------------Welcome to Address Book Program------------ ");
+            AddressDetails addressBook = new AddressDetails();
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+            addressBook.ViewContact();
             Console.Read();
-
         }
+
     }
-}
+    }
